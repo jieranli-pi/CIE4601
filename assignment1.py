e619a73d1a6a4aa91b2def9fdb5fdda5c10232e6
jieranli-pi <73694125+jieranli-pi@users.noreply.github.com>
2021-09-07 15:38:33 +0200

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -6,6 +6,8 @@ a distanceof 2.4384810e10m.  The star has a radius of 2.9246410e8m and a tempe
 """
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
+
 radius_orbit=2.43848*10**10; #m
 radius_star=2.92464*10**8; #m
 Temerature_Kelvin_star=3480; #Kelvin
@@ -37,19 +39,27 @@ k=1.38*10**(-23); #Boltzmann constant
 #wavelength=np.arange(0,60,0.01);#0 to 2.1 um
 
 Scale=radius_star**2/(radius_star+radius_orbit)**2;
-wavelength=np.linspace(0.01,50,num=5999);
+wavelength=np.linspace(0.01,120,num=11999);
 wavelength=wavelength*10**(-6);
 Radiation_flux_planet=((2*np.pi*h*c**2)/(wavelength**5))*(1/(np.exp(h*c/(k*Temperature_Kelvin_planet*wavelength))-1));
-Radiation_flux_star=((2*np.pi*h*c**2)/(wavelength**5))*(1/(np.exp(h*c/(k*Temerature_Kelvin_star*wavelength))-1))*Scale;
+Radiation_flux_star=((2*np.pi*h*c**2)/(wavelength**5))*(1/(np.exp(h*c/(k*Temerature_Kelvin_star*wavelength))-1));
 
-plt.plot(wavelength,Radiation_flux_planet*wavelength, 'r');
-plt.plot(wavelength,Radiation_flux_star*wavelength, 'b');
+plt.plot(wavelength,Radiation_flux_planet*wavelength, 'r', label='from star short wave');
+plt.legend(['from star short wave'])
+plt.plot(wavelength,Radiation_flux_star*wavelength*Scale/4, 'b', label='from TOI700d infra');
+plt.legend()
+plt.xscale('log')
 
-#plt.scatter(Radiation_flux_planet.argmax(axis=0)*10**(-8),np.max(Radiation_flux),s=70,c='k')
-#plt.scatter(Radiation_flux_star.argmax(axis=0)*10**(-8),np.max(Radiation_flux),s=70,c='k')
+peak_wavelength_planet=wavelength[Radiation_flux_planet.argmax(axis=0)];
+peak_wavelength_star=wavelength[Radiation_flux_star.argmax(axis=0)];
+print("The answer to qustion 1(iii) wavelength peak for star is ", peak_wavelength_planet)
+print("The answer to qustion 1(iii) wavelength peak for star is ", peak_wavelength_star)
+
+#plt.scatter(wavelength[Radiation_flux_planet.argmax(axis=0)],np.max(Radiation_flux_planet*wavelength),s=70,c='k')
+#plt.scatter(wavelength[Radiation_flux_star.argmax(axis=0)],np.max(Radiation_flux_star),s=70,c='k')
 plt.xlabel('wavelength (m)')
-plt.ylabel('Radiation flux I')
-plt.title('Red line: absorbed, Blue line emitted')
+plt.ylabel('Radiation flux I * wavelength (W/m^2)')
+plt.title('Radiation against wavelength')
 plt.show()
 
 """
@@ -57,3 +67,14 @@ plt.show()
 a distanceof 2.4384810e10m.  The star has a radius of 2.9246410e8m and a temperature of
 3480 K. Wewill investigate some features of the planet with the global energy balance model
 """
+def func(T):
+    e=1
+    sigma=5.67*10**(-8)
+    alpha=1
+    R=0
+    So=1196.2073589315598
+    return [alpha*sigma*T[0]**4-2*e*sigma*T[1]**4,
+            sigma*T[0]**4 -e*sigma*T[1]**4 -(1-R)*So/4]
+T = fsolve(func, [1, 1])
+print(T)
+
